1ba049c548fb0b51053a6b575108aea24a8d1ae1
Marsel Galimullin <mfgalimullin@yandex.ru>
2017-02-23 19:01:55 +0300

    change .travis\run.sh

--- .travis/run.sh
+++ .travis/run.sh
@@ -10,4 +10,13 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv activate conan
 fi
 
-python build.py
+if [[ "$(uname -s)" == 'Linux' ]]; then
+    CC=$C_COMPILER
+    CXX=$CXX_COMPILER
+fi
+
+mkdir build && cd build
+conan install .. -s build_type=$BUILD_TYPE --build=missing
+cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER
+cmake --build . -- VERBOSE=1
+ctest -V
